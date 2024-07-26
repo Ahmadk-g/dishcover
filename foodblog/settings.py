9f60da4caf44262864d58b36c09d007aa2a31484
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-07-26 11:15:48 +0200

    Create Recipe model and register in Admin site

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$80zf^jnp@)lbg(2_!7k@8nc42n5a^z2avq&48&ibf%q5foduf'
+# SECRET_KEY = 'django-insecure-$80zf^jnp@)lbg(2_!7k@8nc42n5a^z2avq&48&ibf%q5foduf'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Recipe(models.Model):
+    """
+    Stores a single recipe post entry related to :model:'auth.User'.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_poster"
+    )
+    description = models.CharField(max_length=400, blank=True)
+    calories = models.IntegerField()
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    posted_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    
+    class Meta:
+        ordering = ["-posted_on"]
+
+    def __str__(self):
+        return str(self.title)
