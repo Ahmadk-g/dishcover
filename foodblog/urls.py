711ab4fdafaa8d80fdd02c0b3f2413ce30e80ead
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-07-26 16:02:27 +0200

    create generic recipe list view, with reciple_list template

--- foodblog/urls.py
+++ foodblog/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from recipes.views import my_recipes
+from django.urls import path, include
 
 urlpatterns = [
-    path('', my_recipes, name="recipes"),
+    path('', include("recipes.urls"), name="recipes"),
     path('admin/', admin.site.urls),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Recipe
 
 # Create your views here.
-def my_recipes(request):
-    return HttpResponse("Welcome to the food blog!")
+class RecipeList(generic.ListView):
+    model=Recipe
