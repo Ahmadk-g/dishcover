9b6ba666d6bc3e6b9ea94b762256c910afa84193
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-07-29 16:36:14 +0200

    Add base template

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
-from . import views
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home'),
+    path('', views.RecipeList.as_view(), name='Recipes'),
 ]
