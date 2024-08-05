17bd401f3d475bd8472c2f5b00dd8fc671fa085c
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-08-05 16:25:05 +0200

    Add recipe categories field as many-to-many

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,21 +1,25 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Comment
+from .models import Recipe, Ingredient, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'meal_type', 'calories', 'get_ingredients')
-    search_fields = ['title', 'ingredients', 'meal_type']
-    list_filter = ('posted_on', 'meal_type', 'ingredients')
+    list_display = ('title', 'calories', 'get_ingredients', 'get_categories')
+    search_fields = ['title', 'ingredients', 'categories']
+    list_filter = ('posted_on', 'categories', 'ingredients')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('instructions', )
     # To resolves the error for including a manytomanyfield(Ingredient) in list_display
     def get_ingredients(self, obj):
         return ", ".join([ingredient.name for ingredient in obj.ingredients.all()])
-
     get_ingredients.short_description = 'Ingredients'
     
+    def get_categories(self, obj):
+        return ", ".join([cat.name for cat in obj.categories.all()])
+    get_categories.short_description = 'Categories'
+    
 admin.site.register(Ingredient)
+admin.site.register(Category)
 admin.site.register(Comment)
--- recipes/models.py
+++ recipes/models.py
@@ -4,10 +4,27 @@ from django.core.exceptions import ValidationError
 from cloudinary.models import CloudinaryField
 
 
-MEAL_TYPE = ((0, "Breakfast"),(1, "Lunch"), (2, "Dinner"), (3, "Dessert"), (4, "Salad"), (5, "Soup"), (6, "Snack"))
 
 # Create your models here.
 
+class Category(models.Model):
+    name= models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return str(self.name)
+    
+    def clean(self):
+         # Capitalize first letter
+        formatted_name = self.name.title()
+        if Category.objects.filter(name__iexact=formatted_name).exists():
+            raise ValidationError(f"The recipe category '{formatted_name}' already exists.")
+        self.name = formatted_name
+        
+    def save(self, *args, **kwargs):
+        self.full_clean()  # This will call the clean method
+        super().save(*args, **kwargs)
+        
+        
 class Ingredient(models.Model):
     name = models.CharField(max_length=50, unique=True)
     
@@ -36,11 +53,11 @@ class Recipe(models.Model):
         User, on_delete=models.CASCADE, related_name="recipe_poster"
     )
     description = models.CharField(max_length=400, null=False, blank=False)
-    ingredients = models.ManyToManyField(Ingredient, related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipe_ingredients')
     instructions = models.TextField(null=False, blank=False)
     image = CloudinaryField('image', null=False, blank=False)
     image_alt = models.CharField(max_length=100, null=False, blank=False)
-    meal_type = models.IntegerField(choices=MEAL_TYPE, default=0)
+    categories = models.ManyToManyField(Category, related_name='recipe_type')
     calories = models.IntegerField()
     posted_on = models.DateTimeField(auto_now_add=True)
     
