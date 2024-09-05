fd03d8a3151cb0be2c5cac162c8e260081229e42
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-09-05 14:36:36 +0200

    Install CSP app and alter settings to allow site framing

--- foodblog/settings.py
+++ foodblog/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'recipes',
     'home',
     'about',
+    'csp',
 ]
 
 SITE_ID = 1
@@ -98,9 +99,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'csp.middleware.CSPMiddleware',
 ]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+CSP_FRAME_ANCESTORS = ["'self'", "https://ui.dev/amiresponsive"]
 
 ROOT_URLCONF = 'foodblog.urls'
 
